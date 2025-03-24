48018a5fc1e4f486e09dd112a02a296ab5fab2b1
david.matilla <david.matilla@inetum.com>
2025-03-24 18:46:57 +0100

    null_resource

--- modules/blob/main.tf
+++ modules/blob/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_storage_account" "storage_account" {
+resource "null_resource" "storage_account" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
@@ -6,13 +6,13 @@ resource "azurerm_storage_account" "storage_account" {
   account_replication_type = var.storage_account_replication_type
 }
 
-resource "azurerm_storage_container" "storage_container" {
+resource "null_resource" "storage_container" {
   name                  = var.storage_container_name
   storage_account_id    = azurerm_storage_account.storage_account.id 
   container_access_type = var.container_access_type
 }
 
-resource "azurerm_storage_blob" "blob" {
+resource "null_resource" "blob" {
   name                   = var.blob_name
   storage_account_name   = azurerm_storage_account.storage_account.name
   storage_container_name = azurerm_storage_container.storage_container.name
